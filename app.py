45ff3a486dc8359db3033b570621d4b46a9cd2ce
Attila Liptak <atis.liptak@gmail.com>
2020-06-10 16:37:06 +0200

    működő

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+
+engine = create_engine('mysql://drkgwebshop@localhost/merch')
+database = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/admin')
 def admin():
@@ -11,4 +17,4 @@ def index():
     return render_template('index.html', name=name)
     
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
