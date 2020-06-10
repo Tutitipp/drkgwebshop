285ff1bc737b33fdef5f966bf12192b5a0d4c640
Buksii <bencecsengeri@gmail.com>
2020-06-10 16:32:20 +0200

    alap-snap

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/admin')
 def admin():
     return "Kezelőfelület helye"
 @app.route('/')
 def index():
-    name="Attila"
-    return render_template('index.html', name=name)
+    
+    return render_template('index.html')
     
 if __name__ == "__main__":
     app.run()
